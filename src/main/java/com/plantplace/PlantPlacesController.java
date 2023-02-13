6bbbc6397f307cb09debd17a56f507576c8b8eeb
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-12 23:04:45 -0500

    Third try

--- src/main/java/com/plantplace/PlantPlacesController.java
+++ src/main/java/com/plantplace/PlantPlacesController.java
@@ -1,9 +1,10 @@
 package com.plantplace;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-
 public class PlantPlacesController {
 
 	@RequestMapping("/start")
