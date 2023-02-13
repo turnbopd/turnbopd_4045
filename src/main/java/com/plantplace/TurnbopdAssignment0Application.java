78002118df78bd2e0e66298d319e390bd4ed3b2b
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-12 22:06:57 -0500

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/plantplace/TurnbopdAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.plantplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnbopdAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnbopdAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantplace/turnbopd_Assignment0/TurnbopdAssignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantplace.turnbopd_Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnbopdAssignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
